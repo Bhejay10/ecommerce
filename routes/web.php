95be132c11d026798f7fd65d6d487e769814b652
J&P Delivery Services <bhejaygamboalandicho@gmail.com>
2025-04-04 11:24:44 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
